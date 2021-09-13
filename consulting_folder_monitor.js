228c41bb73af35f02970e5465b6bb4255a9b1670
Andre Low <andre-low@users.noreply.github.com>
2021-09-14 01:36:06 +0800

    Refactored to ES5 to combat V8 engine error in Google Apps Script

--- consulting_folder_monitor.js
+++ consulting_folder_monitor.js
@@ -1,22 +1,26 @@
 function checkConsultingFolder() {
-  const folderIdConsulting =   '108hMH7ak3V2I8v5FkvMy2h7aB4QxShKb'; // Consulting folder
-  const folderIdReadyToDraft = '1r-t8rdQv1SD2lmtHhHz0pFwAH2YKIk6R'; // Ready to draft folder
-  const folderIdDrafts =       '1SB1Y_5P2Kc-oIPAvzeIs3aurqIpT4BzP'; // Drafts folder
-  const folderIdArchive =      '1_JVQI0ZjCZ3MJwq1tsGrrTTFCeGr37PY'; // Case and document archive folder
+  // This script is written in ES5 because of an error with the V8 engine when programatically creating time based triggers in Google Apps Script
+
+  // Constants
+  var folderIdConsulting =   '108hMH7ak3V2I8v5FkvMy2h7aB4QxShKb'; // Consulting folder
+  var folderIdReadyToDraft = '1r-t8rdQv1SD2lmtHhHz0pFwAH2YKIk6R'; // Ready to draft folder
+  var folderIdDrafts =       '1SB1Y_5P2Kc-oIPAvzeIs3aurqIpT4BzP'; // Drafts folder
+  var folderIdArchive =      '1_JVQI0ZjCZ3MJwq1tsGrrTTFCeGr37PY'; // Case and document archive folder
 
   // Search for case sheet(s) with 'mpdone' in body but search can have a 5-10 min lag; the MP should input 'mpdone' when done with a case
-  const diagnosedCases = DriveApp.getFolderById(folderIdConsulting).searchFiles("fullText contains 'mpdone'");
+  var diagnosedCases = DriveApp.getFolderById(folderIdConsulting).searchFiles("fullText contains 'mpdone'");
   // Alternatively the MP can shift the file into the 'Ready to Draft' folder which seems to work faster
-  const diagnosedCasesAlt = DriveApp.getFolderById(folderIdReadyToDraft).getFilesByType('application/vnd.google-apps.document');
+  var diagnosedCasesAlt = DriveApp.getFolderById(folderIdReadyToDraft).getFilesByType('application/vnd.google-apps.document');
 
   // Create archive folder with today's date (if it does not already exist)
+  var archiveFolderDate;
   if (diagnosedCases.hasNext() || diagnosedCasesAlt.hasNext()) {
-    const today = Utilities.formatDate(new Date(), 'GMT+8', 'yyyyMMdd'); // Get today's date (yyyyMMdd)
-    const archiveFolder = DriveApp.getFolderById(folderIdArchive);
+    var today = Utilities.formatDate(new Date(), 'GMT+8', 'yyyyMMdd'); // Get today's date (yyyyMMdd)
+    var archiveFolder = DriveApp.getFolderById(folderIdArchive);
     if (archiveFolder.getFoldersByName(today).hasNext()) {
-      var archiveFolderDate = archiveFolder.getFoldersByName(today).next(); // Get the folder with today's date if it exists
+      archiveFolderDate = archiveFolder.getFoldersByName(today).next(); // Get the folder with today's date if it exists
     } else {
-      var archiveFolderDate = archiveFolder.createFolder(today); // Otherwise create a folder with today's date
+      archiveFolderDate = archiveFolder.createFolder(today); // Otherwise create a folder with today's date
     }
   } else {
     // Exit the whole function if no completed cases found
@@ -34,11 +38,11 @@ function checkConsultingFolder() {
 }
 
 function processCaseSheets(fileIterator, archiveFolderDate, folderIdDrafts) {
-  let caseSheet = fileIterator.next();
-  let draftingTemplate = caseSheet.makeCopy(); // Create a copy of the case sheet to act as the drafting template
+  var caseSheet = fileIterator.next();
+  var draftingTemplate = caseSheet.makeCopy(); // Create a copy of the case sheet to act as the drafting template
 
-  const caseRef = caseSheet.getName(); // E.g. 'RV1000-202109-####: Tan Ah Seng'
-  const caseRefTruncated = caseRef.slice(0, 13) + caseRef.slice(18); // E.g. 'RV1000-202109: Tan Ah Seng'
+  var caseRef = caseSheet.getName(); // E.g. 'RV1000-202109-####: Tan Ah Seng'
+  var caseRefTruncated = caseRef.slice(0, 13) + caseRef.slice(18); // E.g. 'RV1000-202109: Tan Ah Seng'
 
   moveFiles(caseSheet.getId(), archiveFolderDate.getId()); // Move original case sheet to the archive folder with today's date
   caseSheet.setName(caseRefTruncated); // Rename the case sheet to remove '-####' from the file name
@@ -58,41 +62,41 @@ function periodicTrigger() {
 }
 
 function terminatePeriodicTrigger() {
-  let triggers = getProjectTriggersByFunctionName('checkConsultingFolder');
-  for (let i = 0; i < triggers.length; ++i) {
+  var triggers = getProjectTriggersByFunctionName('checkConsultingFolder');
+  for (var i = 0; i < triggers.length; ++i) {
     ScriptApp.deleteTrigger(triggers[i]);
   }
 }
 
 function getProjectTriggersByFunctionName(functionName) {
-  return ScriptApp.getProjectTriggers().filter(
-    (trigger) => trigger.getHandlerFunction() === functionName
-  );
+  return ScriptApp.getProjectTriggers().filter(function (trigger) {
+    return trigger.getHandlerFunction() === functionName;
+  });
 }
 
 function moveFiles(sourceFileId, targetFolderId) {
-  let file = DriveApp.getFileById(sourceFileId);
-  let folder = DriveApp.getFolderById(targetFolderId);
+  var file = DriveApp.getFileById(sourceFileId);
+  var folder = DriveApp.getFolderById(targetFolderId);
   file.moveTo(folder);
 }
 
 function weeklyTriggers() {
   // Run once to create the triggers
-  // Start folder monitoring at 6.30pm on Mondays
+  // Start folder monitoring at 7.00pm on Mondays
   ScriptApp.newTrigger('periodicTrigger')
     .timeBased()
     .onWeekDay(ScriptApp.WeekDay.MONDAY)
-    .atHour(18)
-    .nearMinute(30)
+    .atHour(19)
+    .nearMinute(0)
     .inTimezone('Asia/Singapore')
     .create();
 
-  // End folder monitoring at 12.30am on Tuesdays
+  // End folder monitoring at 11.45pm on Mondays
   ScriptApp.newTrigger('terminatePeriodicTrigger')
     .timeBased()
-    .onWeekDay(ScriptApp.WeekDay.TUESDAY)
-    .atHour(0)
-    .nearMinute(30)
+    .onWeekDay(ScriptApp.WeekDay.MONDAY)
+    .atHour(23)
+    .nearMinute(45)
     .inTimezone('Asia/Singapore')
     .create();
 }
